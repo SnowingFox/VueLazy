d701286eef7745640fe3b239ae11caccb2056114
SnowingFox <1503401882@qq.com>
2021-12-01 11:58:16 +0800

    feat(all): init project

---
+++ script/types/index.ts
@@ -0,0 +1,14 @@
+export interface Index {
+  /*
+   * Error message when loading img failed
+   *
+   * @default string
+   */
+  error?: string
+  /*
+   * Url to the lazy loading img
+   *
+   * @default
+   */
+  loading?: string
+}
