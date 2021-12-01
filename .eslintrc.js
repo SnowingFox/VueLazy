9ce775969aa33ae53a67d149b048593e05d8ad66
SnowingFox <1503401882@qq.com>
2021-12-01 11:08:31 +0800

    perf(use eslint & prettier): use eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        jest: true,
+    },
+    "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module"
+    },
+    rules: {
+        'no-unused-vars': 'off',
+    }
+}
